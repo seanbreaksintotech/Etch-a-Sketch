76d07c62e5e6377f6057c64c6193ab2ce5a2e590
seanbreaksintotech <sbyrdie123@gmail.com>
2022-01-27 15:49:10 -0500

    app.js and index.html

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
-const containter = document.getElementById("container")
-const bttnSize = document.getElementById("")
+
+//defining nodes from html
+const container = document.querySelector(".container")
+const bttnSize = document.createElement('button')
+const bttnDraw = document.createElement ('button')
+const bttnContainer = document.querySelector('.buttons')
 
 //function that makes grid from container div
-function makeGrid (col, row){
-    for (let i = 0; i < (col *row); i ++){
-        const div = document.createElement('div')
-        div.style.border = "1px solid black"
-        containter.style.gridTemplateColumns = `repeat (${col}, 1fr)`;
-        containter.style.gridTemplateRows = `repeat ( ${row }, 1fr)`;
-        containter.appendChild(div).classList.add('box')
+function makeGrid (col, rows){
+    //loop for rows and cols
+    for (let i=0; i < (col *rows); i++){
+        
+    const div = document.createElement('div')
+    div.style.border = "1px solid black";
+    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+    container.style.gridTemplateRows = `repeat (${rows}, 1fr)`;
+    container.appendChild(div).classList.add("cell")
+
+    
+
+    };
+};
+    makeGrid (16, 16); 
+
+
+//function that fills cells with color upon clicking bttnDraw
+    function blackDraw(){
+        const cells = container.querySelectorAll('.cell')
+        bttnDraw.textContent ='Draw'
+
+        //click event that leads to hover function 
+        bttnDraw.addEventListener('click', () => {
+            cells.forEach(cell => cell.addEventListener('mouseover', () =>{
+            cell.style.background = 'black';
+        }))
+        }) 
+        bttnContainer.appendChild(bttnDraw).classList.add('btn')
     }
+    blackDraw()
+//
 
 
+    function reSet(){
+        const cells = container.querySelectorAll('.cell')
+        cells.forEach(cell => cell.remove ())
     }
-    makeGrid (16,16)
+    function reSize (){
+        bttnSize.textContent ='Resize'
+        bttnSize.addEventListener('click',  () => {
+            let user = prompt ("Inupt Dimensions")
+            if (user === null)
 
+        })
+        bttnContainer.appendChild(bttnSize).classList.add('btn')
+    
+    }
+    reSize()
+
+    
